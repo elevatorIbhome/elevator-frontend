a19bc15d87ea39f846ab41856662190c3825cab5
rayhan <mohammadrayhanx0@gmail.com>
2025-10-30 23:20:25 +0600

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { NavLink } from 'react-router';
+
+const Navbar = () => {
+    const links = <>
+        <li className='roboto'><NavLink to='/'  className={({ isActive }) => isActive ? "active text-green-800 font-semibold border-b-2" : ""} > Home</NavLink></li>
+
+        <li className='roboto'><NavLink to='/member'  className={({ isActive }) => isActive ? "active text-green-800 font-semibold boreder-b-2 " : ""} >Membership Page</NavLink></li>
+
+        <li className='roboto'><NavLink to='/about'  className={({ isActive }) => isActive ? "active text-green-800 font-semibold border-b-2" : ""} > About</NavLink></li>
+
+        <li className='roboto'><NavLink to='/contact'  className={({ isActive }) => isActive ? "active text-green-800 font-semibold border-b-2" : ""} >Contact</NavLink></li>
+
+        <li className='roboto'><NavLink to='/news'  className={({ isActive }) => isActive ? "active text-green-800 font-semibold border-b-2" : ""} >Latest News</NavLink></li>
+    </>
+    return (
+        <div className="navbar bg-base-100 shadow-sm rounder-lg">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex="-1"
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            {
+                                links
+                            }
+
+                    </ul>
+                </div>
+              <div className='w-16'>
+                 <img src="/src/assets/logo.png" alt="" />
+              </div>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {
+                        links
+                    }
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a >text</a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/Pages/Home/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            I am banner
+            <img src="../../assets/logo.png" alt="" />
+            <img src="/src/assets/logo.png" alt="" />
+            HIii bye bye
+        </div>
+    );
+};
+
+export default Banner;
