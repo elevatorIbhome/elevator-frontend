10d0a6525b30888b587ea05d7517bd66283a2307
jawadibrahim <jawadibrahimshopnil01@gmail.com>
2025-12-12 04:08:03 +0600

    changed: intent api

--- src/StripePayment/Payment.jsx
+++ src/StripePayment/Payment.jsx
@@ -20,7 +20,7 @@ const Payment = () => {
     useEffect(()=>{
         console.log("token: ", user.accessToken);
         // Create PaymentIntent as soon as the page loads
-        fetch("https://elevator-backend.vercel.app/api/create-payment-intent", {
+        fetch("https://elevator-backend.vercel.app/create-payment-intent", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
